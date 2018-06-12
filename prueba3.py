a4d77d032e77a14d016e27fa8d910d623fa99888
GabrielBarrantes <gabrielenrique.barrantes@gmail.com>
2018-06-12 14:23:59 -0600

    Update prueba3.py

--- prueba3.py
+++ prueba3.py
@@ -6,6 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
+import images_rc
 from PyQt5 import QtCore, QtGui, QtWidgets
 from pygame import mixer # Load the required library
 
@@ -13,45 +14,98 @@ class Ui_Radio(object):
     def setupUi(self, Radio):
         Radio.setObjectName("Radio")
         Radio.resize(640, 480)
-        self.horizontalLayoutWidget = QtWidgets.QWidget(Radio)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(9, 89, 621, 41))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
-        self.gridLayoutWidget = QtWidgets.QWidget(Radio)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(9, 9, 621, 78))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
-        self.gridLayout.setObjectName("gridLayout")
-        self.MP3Button = QtWidgets.QCommandLinkButton(self.gridLayoutWidget)
-        self.MP3Button.setObjectName("MP3Button")
+        Radio.setStyleSheet("background-color: rgb(138, 149, 151);\n"
+"border-color: rgb(138, 149, 151);")
+        self.tabWidget = QtWidgets.QTabWidget(Radio)
+        self.tabWidget.setGeometry(QtCore.QRect(0, 0, 640, 480))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.tabWidget.sizePolicy().hasHeightForWidth())
+        self.tabWidget.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        self.tabWidget.setFont(font)
+        self.tabWidget.setStyleSheet("background-color: rgb(138, 149, 151);\n"
+"")
+        self.tabWidget.setObjectName("tabWidget")
+        self.home = QtWidgets.QWidget()
+        self.home.setObjectName("home")
+        self.dateTimeEdit = QtWidgets.QDateTimeEdit(self.home)
+        self.dateTimeEdit.setGeometry(QtCore.QRect(70, 50, 511, 321))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.dateTimeEdit.setFont(font)
+        self.dateTimeEdit.setObjectName("dateTimeEdit")
+        self.tabWidget.addTab(self.home, "")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.FrecuencyDisplay = QtWidgets.QLCDNumber(self.tab)
+        self.FrecuencyDisplay.setGeometry(QtCore.QRect(110, 40, 461, 191))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.FrecuencyDisplay.sizePolicy().hasHeightForWidth())
+        self.FrecuencyDisplay.setSizePolicy(sizePolicy)
+        self.FrecuencyDisplay.setObjectName("FrecuencyDisplay")
+        self.horizontalSlider = QtWidgets.QSlider(self.tab)
+        self.horizontalSlider.setGeometry(QtCore.QRect(20, 360, 571, 61))
+        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalSlider.setObjectName("horizontalSlider")
+        self.volumeSliderRadio = QtWidgets.QSlider(self.tab)
+        self.volumeSliderRadio.setGeometry(QtCore.QRect(20, 30, 61, 301))
+        self.volumeSliderRadio.setOrientation(QtCore.Qt.Vertical)
+        self.volumeSliderRadio.setObjectName("volumeSliderRadio")
+        self.tabWidget.addTab(self.tab, "")
+        self.mp3 = QtWidgets.QWidget()
+        self.mp3.setObjectName("mp3")
+        self.previousButton = QtWidgets.QPushButton(self.mp3)
+        self.previousButton.setGeometry(QtCore.QRect(230, 220, 71, 71))
+        font = QtGui.QFont()
+        self.previousButton.setFont(font)
+        self.previousButton.setStyleSheet("image: url(:/images/back.png);\n"
+"background-image: url(:/images/blue.png);")
+        self.previousButton.setText("")
+        self.previousButton.setObjectName("previousButton")
+        self.playButton = QtWidgets.QPushButton(self.mp3)
+        self.playButton.setGeometry(QtCore.QRect(310, 220, 71, 71))
+        font = QtGui.QFont()
+        self.playButton.setFont(font)
+        self.playButton.setStyleSheet("background-image: url(:/images/blue.png);\n"
+"image: url(:/images/play.png);\n"
+"")
+        self.playButton.setText("")
+        self.playButton.setObjectName("playButton")
         ######################################################################
-        self.MP3Button.clicked.connect(self.play)
+        self.playButton.clicked.connect(self.play)
         ######################################################################
-        self.gridLayout.addWidget(self.MP3Button, 1, 0, 1, 1)
-        self.RadioButton = QtWidgets.QCommandLinkButton(self.gridLayoutWidget)
-        self.RadioButton.setObjectName("RadioButton")
-        self.gridLayout.addWidget(self.RadioButton, 0, 0, 1, 1)
-        self.verticalLayoutWidget = QtWidgets.QWidget(Radio)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(30, 140, 21, 321))
-        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.verticalSlider = QtWidgets.QSlider(self.verticalLayoutWidget)
-        self.verticalSlider.setOrientation(QtCore.Qt.Vertical)
-        self.verticalSlider.setObjectName("verticalSlider")
-        self.verticalLayout.addWidget(self.verticalSlider)
+        self.nextButton = QtWidgets.QPushButton(self.mp3)
+        self.nextButton.setGeometry(QtCore.QRect(390, 220, 61, 71))
+        font = QtGui.QFont()
+        self.nextButton.setFont(font)
+        self.nextButton.setStyleSheet("background-image: url(:/images/blue.png);\n"
+"image: url(:/images/forward.png);")
+        self.nextButton.setText("")
+        self.nextButton.setObjectName("nextButton")
+        self.volumSliderMP3 = QtWidgets.QSlider(self.mp3)
+        self.volumSliderMP3.setGeometry(QtCore.QRect(15, 29, 71, 301))
+        self.volumSliderMP3.setOrientation(QtCore.Qt.Vertical)
+        self.volumSliderMP3.setObjectName("volumSliderMP3")
+        self.textBrowser = QtWidgets.QTextBrowser(self.mp3)
+        self.textBrowser.setGeometry(QtCore.QRect(120, 151, 451, 41))
+        self.textBrowser.setObjectName("textBrowser")
+        self.tabWidget.addTab(self.mp3, "")
 
         self.retranslateUi(Radio)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(Radio)
 
     def retranslateUi(self, Radio):
         _translate = QtCore.QCoreApplication.translate
         Radio.setWindowTitle(_translate("Radio", "Radio Incrustada"))
-        self.MP3Button.setText(_translate("Radio", "MP3"))
-        self.RadioButton.setText(_translate("Radio", "Radio"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.home), _translate("Radio", "Home"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("Radio", "Radio"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.mp3), _translate("Radio", "MP3"))
 
     def play(self, Radio):
         mixer.init()
@@ -59,6 +113,4 @@ class Ui_Radio(object):
         mixer.music.load('test.mp3')
         mixer.music.play()
         #mixer.music.stop()
-
-
 
