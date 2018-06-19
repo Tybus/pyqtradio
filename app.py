c97b2151cf0b883aa7d3b4a57c1276b5940134ab
GabrielBarrantes <gabrielenrique.barrantes@gmail.com>
2018-06-18 23:09:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import sys
+playListPath = '/home/gab/Qt5/RadioGUI/pyqtradio-master/'
+
 
 from PyQt5.QtWidgets import QApplication, QWidget, QDialog
-from radio import Ui_gui
+from radio5 import Ui_gui
 
 class AppWindow(QDialog):
 	def __init__(self):
 		super().__init__()
-		self.ui = Ui_gui()
+		self.ui = Ui_gui(playListPath)
 		self.ui.setupUi(self)
-		self.show()
+		self.showFullScreen()
 
 app = QApplication(sys.argv)
 w = AppWindow()
